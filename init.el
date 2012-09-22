6260bc60e51ffc43ccf33a32560d86aa01211310
Chun-wei Kuo <Dendoh@gmail.com>
2012-09-22 22:04:20 +0800

    Disable the toolbar in X windows

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d")
 
+; Disable the toolbar in X windows
+(if window-system
+  (tool-bar-mode -1))
+
 ; Color Theme
 (require 'color-theme)
 (color-theme-initialize)
@@ -8,3 +12,4 @@
 (add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized")
 (require 'color-theme-solarized)
 (color-theme-solarized-dark)
+
